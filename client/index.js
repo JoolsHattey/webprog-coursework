7514f6a7c044fb8dc86f6874edd49d9afd515fa3
Jools Hattey <jools.hg@gmail.com>
2020-02-05 18:59:47 +0000

    Input elements
    Create input elements for question based on input type

--- client/index.js
+++ client/index.js
@@ -1,44 +1,118 @@
+let questions;
+
 async function getQuestions() {
     const response = await fetch("/getquestions");
 
     const yiss = await response.json();
 
-    displayQuestions(yiss);
+    questions = yiss;
+
+    const title = document.querySelector("#title");
+    title.textContent=questions.name;
+    body.appendChild(title);
 }
 
-function displayQuestions(questions) {
+function displayQuestions() {
     const body = document.querySelector("main");
+    
 
     createQuestion(questions.questions[currentQuestion], body);
-
-    //questions.questions.forEach(element => createQuestion(element, body));
 }
 
 function createQuestion(question, bodyElement) {
-    const newQ = document.querySelector("#questionContainer")
+    
+    const q = document.querySelector("#questionContainer");
+    const newQ = document.createElement("div");
 
-    const qTitle = document.createElement("h2");
+    const qTitle = document.createElement("h3");
     const qTitleContent = document.createTextNode(question.text);
     qTitle.appendChild(qTitleContent);
 
-    //const q
-
-    if(newQ.children[0]) {
-        newQ.children[0].remove();}
+    
 
     newQ.appendChild(qTitle);
 
-    bodyElement.appendChild(newQ);
+    let input;
+
+    switch(question.type) {
+        case "text":
+            input = createTextInput()
+            break;
+        case "number":
+            input = createNumberInput();
+            break;
+        case "single-select":
+            input = createSingleSelectInput(question.options);
+            break;
+        case "multi-select":
+            input = createMultiSelectInput(question.options);
+            break;
+    }
+
+    if(q.children[0]) {
+        q.children[0].remove();
+    }
+
+    newQ.appendChild(input);
+
+    q.appendChild(newQ);
 }
+
+function createTextInput() {
+    return document.createElement("input");
+}
+function createNumberInput() {
+    const inputElement = document.createElement("input");
+    inputElement.setAttribute("type", "number");
+    return inputElement;
+}
+function createMultiSelectInput(options) {
+    const inputElement = document.createElement("div");
+    options.forEach(opt => {
+        const optionContainer = document.createElement("div");
+        const option = document.createElement("input");
+        option.setAttribute("type", "checkbox");
+        option.setAttribute("name", "name");
+        option.setAttribute("id", opt);
+
+        const label = document.createElement("label");
+        label.setAttribute("for", opt)
+        label.textContent = opt
+        optionContainer.appendChild(option);
+        optionContainer.appendChild(label);
+        inputElement.appendChild(optionContainer);
+    });
+    return inputElement;
+}
+function createSingleSelectInput(options) {
+    const inputElement = document.createElement("div");
+    options.forEach(opt => {
+        const optionContainer = document.createElement("div");
+        const option = document.createElement("input");
+        option.setAttribute("type", "radio");
+        option.setAttribute("name", "name");
+        option.setAttribute("value", opt);
+        option.setAttribute("id", opt);
+
+        const label = document.createElement("label");
+        label.setAttribute("for", opt)
+        label.textContent = opt
+        optionContainer.appendChild(option);
+        optionContainer.appendChild(label);
+        inputElement.appendChild(optionContainer);
+    });
+    return inputElement;
+}
+
+getQuestions();
 let currentQuestion = -1;
 const nextButton = document.querySelector("#next");
 nextButton.addEventListener("click", event => {
     currentQuestion++;
-    getQuestions();
+    displayQuestions();
 });
 const prevButton = document.querySelector("#prev");
 prevButton.addEventListener("click", event => {
     currentQuestion++;
-    getQuestions();
+    displayQuestions();
 });
-//getQuestions();
