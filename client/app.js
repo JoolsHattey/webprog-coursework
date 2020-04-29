3148a645ccbfee3956f43427d618a28a1471d93e
Jools Hattey <jools.hg@gmail.com>
2020-04-29 16:51:59 +0100

    render function

--- client/app.js
+++ client/app.js
@@ -7,7 +7,8 @@ import { HomePage } from './views/home-page/index.js';
 import { AdminPage } from './views/admin-page/index.js';
 import { QuizPage } from './views/quiz-page/index.js';
 import { AppBar } from './components/app-bar/index.js';
-
+import { ProgressSpinner } from './components/progress-spinner/progress-spinner.component.js';
+import { Icon } from './components/icon/icon.component.js';
 
 
 /********************************************
@@ -36,5 +37,16 @@ authObservable.subscribe({next: x => {
  * @return {Element}
  */
 export function $(ctx, queryName) {
-    return ctx.shadowRoot.querySelector(queryName);
+    if(ctx.shadowRoot) {
+        return ctx.shadowRoot.querySelector(queryName);
+    }
+    return ctx.querySelector(queryName);
+}
+export async function $r(elName, template) {
+    const el = document.createElement(elName);
+    if(template) {
+        let res = await fetch(template);
+        el.innerHTML = await res.text();
+    }
+    return el;
 }
