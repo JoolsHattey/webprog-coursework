68a9e257a37dddd1f44687be8015f9b25318d089
Jools Hattey <jools.hg@gmail.com>
2020-05-21 23:11:37 +0100

    fixed bar chart sizing

--- client/components/chart/bar-chart.component.js
+++ client/components/chart/bar-chart.component.js
@@ -1,11 +1,23 @@
 'use strict';
 
-import { Chart } from './chart.component.js';
+import { Component } from '../component.js';
 
-export class BarChart extends Chart {
+export class BarChart extends Component {
   constructor(data) {
-    super();
+    super({
+      stylesheet: '/components/chart/chart.component.css',
+    });
+    this.initElement(data);
     this.drawChart(data);
+  }
+
+  initElement(data) {
+    this.canvas = document.createElement('canvas');
+    this.container.append(this.canvas);
+    const numItems = Object.keys(data).length;
+    this.canvas.width = 600;
+    this.canvas.height = 140 * (numItems - 1) + 100;
+    this.ctx = this.canvas.getContext('2d');
   }
 
   drawChart(data) {
@@ -22,13 +34,13 @@ export class BarChart extends Chart {
 
   drawBar(label, value, i) {
     this.ctx.fillStyle = '#439889';
-    this.ctx.fillRect(0, i * 70, value / this.total * 300, 50);
+    this.ctx.fillRect(0, i * 140, value / this.total * 600, 100);
     this.ctx.fillStyle = 'black';
-    this.ctx.font = '1.5rem Roboto';
+    this.ctx.font = '2.25rem Roboto';
     this.ctx.textAlign = 'left';
-    this.ctx.fillText(label, 10, i * 70 + 30);
+    this.ctx.fillText(label, 20, i * 140 + 62);
     this.ctx.textAlign = 'right';
-    this.ctx.fillText(`${(value / this.total * 100).toFixed(1)}%`, 300, i * 70 + 30);
+    this.ctx.fillText(`${(value / this.total * 200).toFixed(1)}%`, 600, i * 140 + 62);
   }
 }
 window.customElements.define('bar-chart', BarChart);
