e8dfd11e8307db83eee83789176fcee4fa047385
Jools Hattey <jools.hg@gmail.com>
2020-05-27 15:34:22 +0100

    quiz not found page

--- client/modules/quiz-page/quiz-page.component.js
+++ client/modules/quiz-page/quiz-page.component.js
@@ -2,6 +2,7 @@
 
 import { Component } from '../../components/component.js';
 import { Quiz } from './quiz/quiz.component.js';
+import { Card } from '../../components/card/card.component.js';
 
 export class QuizPage extends Component {
   constructor(req) {
@@ -13,11 +14,18 @@ export class QuizPage extends Component {
 
   async initElement(req) {
     this.container.classList.add('page');
-    const request = await fetch(`/api/questionnaires/${req.params.quizID}`);
-    const quizData = await request.json();
-    const q = new Quiz(req.params.quizID, quizData);
-    this.container.appendChild(q);
-    document.title = quizData.name;
+    const res = await fetch(`/api/questionnaires/${req.params.quizID}`);
+    if (res.ok) {
+      const quizData = await res.json();
+      const q = new Quiz(req.params.quizID, quizData);
+      this.container.appendChild(q);
+      document.title = quizData.name;
+    } else {
+      const quizNotFoundCard = new Card();
+      quizNotFoundCard.classList.add('quizNotFoundCard');
+      quizNotFoundCard.createTitle('Quiz not found');
+      this.container.append(quizNotFoundCard);
+    }
   }
 }
 
